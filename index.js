c6ff8a10e6292bdd25a7b2fa125ee15e4384b6b5
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-01 14:17:03 -0700

    Added test specifications and added function to render svg text for the text and text color of svg

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 const shapes = require('./lib/shapes.js');
 const inquirer = require('inquirer');
+const shapeColor = require('./lib/colorFill.js');
+const textColor = require('./lib/textFill.js');
+
+
+
+
