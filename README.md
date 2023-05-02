e4018ed42fec15119cd1ff8659bdd45807ee8d9b
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-01 21:34:42 -0700

    Finished the function to create svg file and render image in browser. Added readme file

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # svg-generator
+
+A Node.js application that presents the user with different prompts for information for an SVG logo, and renders it into an svg file when prompts are completed. Tests using Jest have been utilized to make sure that the code creating the svg syntax is efficient and working properly.
+
+## Motivation
+
+As a developer, it is important to be efficient with time. Having an application that can generator a logo almost instantly, without having to continuously re-write the code, every time is invaluable. 
+
+## Technologies
+
+Project was created with:
+* Node.js
+* JavaScript
+* Jest
+
+## Installation
+
+To run this project, pull it from Github to your local machine using your terminal.
+Make sure you have node.js installed. 
+Go to index.js and run node.js in that terminal. 
+   
+#### Steps: 
+
+1. Open Github
+2. Navigate to the 'Madilyn Cooper/svg-generator' repository. 
+3. Click the green button on the page that says "code."
+4. Copy the link under SSH. 
+5. Open your terminal and type the following keys.
+
+```
+$ cd <any folder you want this repository to go to>
+$ git clone <paste the copied link>
+```
+
+## Walkthrough Video: 
+
+https://watch.screencastify.com/v/7iyqlSEqckyY7VjQiqlE
+
+ ## Screenshot:
+<img width="924" alt="Screen Shot 2023-05-01 at 9 29 00 PM" src="https://user-images.githubusercontent.com/124405920/235579574-61073a92-793c-4b7f-baa1-cc5a0ea4f232.png">
+<img width="924" alt="Screen Shot 2023-05-01 at 9 28 29 PM" src="https://user-images.githubusercontent.com/124405920/235579610-7ddf07e1-3495-4a1c-a310-4d59e5962d40.png">
+<img width="733" alt="Screen Shot 2023-05-01 at 9 28 16 PM" src="https://user-images.githubusercontent.com/124405920/235579640-8a9ab8ce-f791-46db-aa83-52dfd138556b.png">
+
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,83 @@
 const fs = require('fs');
 const shapes = require('./lib/shapes.js');
 const inquirer = require('inquirer');
-const shapeColor = require('./lib/colorFill.js');
-const textColor = require('./lib/textFill.js');
 
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'What would you like the logo\'s text to be? You may type up to three characters:',
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color would you like the text color to be? Enter a color keyword OR a hexadecimal:',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for you logo:',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color would you like the shape to be? Enter a color keyword OR a hexadecimal:',
+        },
+
+    ])
+    .then((answers) => {
+        const Triangle = shapes.Triangle;
+        const Square = shapes.Square;
+        const Circle = shapes.Circle;
+
+        if (answers.shape === 'triangle') {
+            const shape = new Triangle();
+
+            shape.setColor(`${answers.shapeColor}`);
+            shape.setText(`${answers.textColor}`, `${answers.text}`);
+
+            const triangleText = shape.textSvg();
+            const triangleColor = shape.render();
+
+            let logoSvg = `<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'>
+        ${triangleColor}
+        ${triangleText}
+    </svg>`
+            fs.writeFile('logo.svg', `${logoSvg}`, (err) => console.log('Generated logo.svg!'));
+        }
+        else if (answers.shape === 'square') {
+            const shapeTwo = new Square();
+
+            shapeTwo.setColor(`${answers.shapeColor}`);
+            shapeTwo.setText(`${answers.textColor}`, `${answers.text}`);
+
+            const squareText = shapeTwo.textSvg();
+            const squareColor = shapeTwo.render();
+
+            const logoSvg = `<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'>
+        ${squareColor}
+        ${squareText}
+    </svg>`
+            fs.writeFile('logo.svg', `${logoSvg}`, (err) => console.log('Generated logo.svg!'));
+        }
+        else if (answers.shape === 'circle') {
+            const shapeThree = new Circle();
+
+            shapeThree.setColor(`${answers.shapeColor}`);
+            shapeThree.setText(`${answers.textColor}`, `${answers.text}`);
+
+            const circleText = shapeThree.textSvg();
+            const circleColor = shapeThree.render();
+
+            const logoSvg = `<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'>
+        ${circleColor}
+        ${circleText}
+    </svg>`
+            fs.writeFile('logo.svg', `${logoSvg}`, (err) => console.log('Generated logo.svg!'));
+        }
+    });
 
 
